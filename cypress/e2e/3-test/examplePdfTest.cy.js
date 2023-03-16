924cc5fceb27cf55a3a5a4fdef3c2f161a195d8d
Erwin Gabriel Torres Sangama <ergatosa.95@gmail.com>
2023-03-16 08:50:16 -0500

    Corriendo Bien

--- cypress/e2e/3-test/examplePdfTest.cy.js
+++ cypress/e2e/3-test/examplePdfTest.cy.js
@@ -4,9 +4,16 @@
 describe("cy.task", () => {
     //cy.task -> es para ejucutar un evento,metodo de node.js
     //En este caso estamos llamando al metodo comparePdfOnePage que se encuentra en n
-    it('Comparar version 1 PDFs',() => {
-        cy.task('comparePdfOnePage', { actual: "actualPdf.pdf", baseline: "baselinePdf.pdf" })
-            .should('equal', true)
+    it('Comparar version 1 PDFs', () => {
+        cy.task('comparePdfOnePage',
+            {
+                actual: "archivoO.pdf",
+                baseline: "baselineM.pdf"
+            })
+            .should('equals', true)
+            
     })
+
+
 
 })
